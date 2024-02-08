c049644b5a08c2c1a6e34e1cbffe3aecc4fe66f5
Shivam maurya <shivamaurya191002@gmail.com>
2024-02-08 14:18:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import "./index.css";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [Step, setStep] = useState(1);
+  const [open, setOpen] = useState(true);
+  const handleClickNext = () => {
+    setStep((s) => s + 1);
+  };
+  const handleClickPrevious = () => {
+    setStep((s) => s - 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="close" onClick={() => setOpen(!open)}>
+        &times;
+      </div>
+      {open && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${Step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${Step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${Step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+          <p className="message">
+            Step {Step}:{messages[Step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              disabled={Step === 1}
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleClickPrevious}
+            >
+              Previous
+            </button>
+            <button
+              disabled={Step === 3}
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleClickNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
-
-export default App;
